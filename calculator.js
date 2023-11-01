3324ef10f02aace838e0fb2346b623200a0efcdd
ArtanaFio <125154911+ArtanaFio@users.noreply.github.com>
2023-11-01 15:16:53 -0400

    Create operate and pressOperator functions
    Removed addArray, subtractArray, sumation, and deduction. Created the
    pressOperator function to push numbers into the calculateArray to
    perform operations. Created the operate function to listen for each
    operator button (+, -, *, /) and once the equal button is pressed, will
    perform the associated operation. Currently cannot perform a different
    operation in succession.

--- calculator.js
+++ calculator.js
@@ -1,152 +1,169 @@
 const calculator = document.getElementById("calc");
-const displayScreen = document.getElementById("screen");
-const allButtons = document.querySelector(".all-buttons");
-const allOfTheButtons = document.querySelectorAll(".button");
-const numberSection = document.getElementById("nums");
-const numberButton = document.querySelectorAll(".num");
+        const displayScreen = document.getElementById("screen");
+        const allButtons = document.querySelector(".all-buttons");
+        const allOfTheButtons = document.querySelectorAll(".button");
+        const numberSection = document.getElementById("nums");
+        const numberButton = document.querySelectorAll(".num");
+        const section = document.querySelector('section');
+        const equationExpression = document.getElementById('expression');
 
-const firstRow = document.querySelector(".first");
-const secondRow = document.querySelector(".second");
-const thirdRow = document.querySelector(".third");
-const fourthRow = document.querySelector(".fourth");
+        const firstRow = document.querySelector(".first");
+        const secondRow = document.querySelector(".second");
+        const thirdRow = document.querySelector(".third");
+        const fourthRow = document.querySelector(".fourth");
 
-const oneButton= document.querySelector(".one");
-const twoButton= document.querySelector(".two");
-const threeButton= document.querySelector(".three");
-const fourButton= document.querySelector(".four");
-const fiveButton= document.querySelector(".five");
-const sixButton= document.querySelector(".six");
-const sevenButton= document.querySelector(".seven");
-const eightButton= document.querySelector(".eight");
-const nineButton= document.querySelector(".nine");
-const zeroButton= document.querySelector(".zero");
+        const oneButton= document.querySelector(".one");
+        const twoButton= document.querySelector(".two");
+        const threeButton= document.querySelector(".three");
+        const fourButton= document.querySelector(".four");
+        const fiveButton= document.querySelector(".five");
+        const sixButton= document.querySelector(".six");
+        const sevenButton= document.querySelector(".seven");
+        const eightButton= document.querySelector(".eight");
+        const nineButton= document.querySelector(".nine");
+        const zeroButton= document.querySelector(".zero");
         
-const operationButton = document.querySelectorAll(".opr");
-const column = document.getElementById("ops");
-const clearButton = document.getElementById("clear");
-const plusButton = document.getElementById("add");
-const minusButton = document.getElementById("sub");
-const multiplyButton = document.getElementById("mult");
-const divideButton = document.getElementById("divi");
-const equalButton = document.getElementById("eql");
+        const nonNumbericalButton = document.querySelectorAll(".non");
+        const operationButton = document.querySelectorAll(".opr");
+        const column = document.getElementById("ops");
+        const clearButton = document.getElementById("clear");
+        const plusButton = document.getElementById("add");
+        const minusButton = document.getElementById("sub");
+        const multiplyButton = document.getElementById("mult");
+        const divideButton = document.getElementById("divi");
+        const equalButton = document.getElementById("eql");
 
-const numberArray = [];
-const addArray = [];
-const subtractArray = [];
-let numberString;
-let sumation;
-let deduction;
+        const numberArray = [];
+        const calculateArray = [];
+        let numberString;
 
-displayScreen.textContent = '0';
+        displayScreen.textContent = '0';
 
-function pressAnyButton() {
-    allOfTheButtons.forEach((button) => {
-        button.addEventListener('mousedown', () => {
-            button.style.borderTop = "0.4vw solid darkgrey"; 
-            button.style.borderLeft = "0.4vw solid lightgrey"; 
-            button.style.borderRight = "0.4vw solid transparent"; 
-            button.style.borderBottom = "0.4vw solid transparent";
-        });
+        function pressAnyButton() {
+            allOfTheButtons.forEach((button) => {
+                button.addEventListener('mousedown', () => {
+                    button.style.borderTop = "0.4vw solid darkgrey"; 
+                    button.style.borderLeft = "0.4vw solid lightgrey"; 
+                    button.style.borderRight = "0.4vw solid transparent"; 
+                    button.style.borderBottom = "0.4vw solid transparent";
+                });
 
-        button.addEventListener('mouseup', () => {
-            button.style.borderTop = "0.4vw solid transparent";
-            button.style.borderLeft = "0.4vw solid transparent";
-            button.style.borderRight = "0.4vw solid lightgrey";
-            button.style.borderBottom = "0.4vw solid darkgrey";
-        })
-    });
-};
-pressAnyButton();
+                button.addEventListener('mouseup', () => {
+                    button.style.borderTop = "0.4vw solid transparent";
+                    button.style.borderLeft = "0.4vw solid transparent";
+                    button.style.borderRight = "0.4vw solid lightgrey";
+                    button.style.borderBottom = "0.4vw solid darkgrey";
+                })
+            });
+        };
+        pressAnyButton();
 
-function enterNumber() {
-    numberButton.forEach((button) => {
-        button.addEventListener('mousedown', () => {
-            numberArray.push(parseInt(button.textContent));
-            numberString = parseInt(numberArray.join(''));
-            displayScreen.textContent = numberString;
-        });
-    });
-};
-enterNumber();
+        function enterNumber() {
+            numberButton.forEach((button) => {
+                button.addEventListener('mousedown', () => {
+                    numberArray.push(parseInt(button.textContent));
+                    numberString = parseInt(numberArray.join(''));
+                    displayScreen.textContent = numberString;
+                });
+            });
+        };
+        enterNumber();
+
+        function pressOperator() {
+            operationButton.forEach((button) => {
+                button.addEventListener('mousedown', () => {
+                    numberArray.length = 0;
+                    calculateArray.push(parseInt(numberString));
+                });
+            });
+
+            equalButton.addEventListener('mousedown', () => {
+                numberArray.length = 0;
+                calculateArray.push(parseInt(numberString));
+                numberString = '';
+                console.log("calculateArray elements: " + calculateArray);
+            });
+
+            
+        };
+        pressOperator();
+
+        function operate() {
+            nonNumbericalButton.forEach((button) => {
+                button.addEventListener('mousedown', () => {
+                    if (button.id == "add") {
+                        equalButton.addEventListener('mousedown', () => {
+                            const add = calculateArray.reduce(function(a, b) {
+                                console.log(`${a} + ${b} = ${a + b}`);
+                                return a + b;
+                            });
+                            displayScreen.textContent = add;
+                            calculateArray.length = 0;
+                        });
+                    } else if (button.id == "sub") {
+                        equalButton.addEventListener('mousedown', () => {
+                            const subtract = calculateArray.reduce(function(a, b) {
+                                console.log(`${a} - ${b} = ${a - b}`);
+                                return a - b;
+                            });
+                            displayScreen.textContent = subtract;
+                            calculateArray.length = 0;
+                        });
+                    } else if (button.id == "mult") {
+                        equalButton.addEventListener('mousedown', () => {
+                            const multiply = calculateArray.reduce(function(a, b) {
+                                console.log(`${a} * ${b} = ${a * b}`);
+                                return a * b;
+                            });
+                            displayScreen.textContent = multiply;
+                            calculateArray.length = 0;
+                        });
+                    } else if (button.id == "divi") {
+                        equalButton.addEventListener('mousedown', () => {
+                            const divide = calculateArray.reduce(function(a, b) {
+                                console.log(`${a} / ${b} = ${a / b}`);
+                                return a / b;
+                            });
+                            displayScreen.textContent = divide;
+                            calculateArray.length = 0;
+                        });
+                    } else if (button.id == "clear") {
+                        numberArray.length = 0;
+                        calculateArray.length = 0;
+                        displayScreen.textContent = "0";
+                        console.log("Screen cleared");
+                    } else if (button.id == "eql") {
+                        displayScreen.textContent = numberString;
+                        console.log("pressed =")
+                    }
+                });
+            });
+        };
+        operate();
 
 
-function add() {
-    plusButton.addEventListener('mousedown', () => {
-        numberArray.length = 0;
-        addArray.push(displayScreen.textContent);
-        console.log("addArray: " + addArray);
-    });
-
-    equalButton.addEventListener('mousedown', () => {
-        addArray.push(displayScreen.textContent);
-        console.log("addArray: " + addArray);
-        numberArray.length = 0;
-        sumation = addArray.reduce(function(a, b) {
-            return parseInt(a) + parseInt(b);
-        });
-        console.log("sum: " + sumation);
-        
-        displayScreen.textContent = sumation;
-        addArray.length = 0;
-    });
-};
-add();
-
-function subtract() {
-    minusButton.addEventListener('mousedown', () => {
-        numberArray.length = 0;
-        subtractArray.push(displayScreen.textContent);
-        console.log("subtractArray: " + subtractArray);
-    });
-
-    equalButton.addEventListener('mousedown', () => {
-        subtractArray.push(displayScreen.textContent);
-        console.log("subtractArray: " + subtractArray);
-        numberArray.length = 0;
-        deduction = subtractArray.reduce(function(a, b) {
-            return parseInt(a) - parseInt(b);
-        });
-        console.log("subtraction: " + deduction);
-
-        displayScreen.textContent = deduction;
-        subtractArray.length = 0;
-    });
-};
-subtract();
-
-
-clearButton.addEventListener('click', () => {
-    numberArray.length = 0;
-    addArray.length = 0;
-    subtractArray.length = 0;
-    displayScreen.textContent = "0";
-    console.log("Screen cleared");
-    console.log(numberArray);
-    console.log(numberString);
-});
-
-
-calculator.appendChild(displayScreen);
-calculator.appendChild(allButtons);
-allButtons.appendChild(numberSection);
-allButtons.appendChild(column);
-numberSection.appendChild(firstRow);
-numberSection.appendChild(secondRow);
-numberSection.appendChild(thirdRow);
-numberSection.appendChild(fourthRow);
-column.appendChild(clearButton);
-column.appendChild(plusButton);
-column.appendChild(minusButton);
-column.appendChild(multiplyButton);
-column.appendChild(divideButton);
-column.appendChild(equalButton);
-firstRow.appendChild(oneButton);
-firstRow.appendChild(twoButton);
-firstRow.appendChild(threeButton);
-secondRow.appendChild(fourButton);
-secondRow.appendChild(fiveButton);
-secondRow.appendChild(sixButton);
-thirdRow.appendChild(sevenButton);
-thirdRow.appendChild(eightButton);
-thirdRow.appendChild(nineButton);
-fourthRow.appendChild(zeroButton);
+        calculator.appendChild(displayScreen);
+        calculator.appendChild(allButtons);
+        allButtons.appendChild(numberSection);
+        allButtons.appendChild(column);
+        numberSection.appendChild(firstRow);
+        numberSection.appendChild(secondRow);
+        numberSection.appendChild(thirdRow);
+        numberSection.appendChild(fourthRow);
+        column.appendChild(clearButton);
+        column.appendChild(plusButton);
+        column.appendChild(minusButton);
+        column.appendChild(multiplyButton);
+        column.appendChild(divideButton);
+        column.appendChild(equalButton);
+        firstRow.appendChild(oneButton);
+        firstRow.appendChild(twoButton);
+        firstRow.appendChild(threeButton);
+        secondRow.appendChild(fourButton);
+        secondRow.appendChild(fiveButton);
+        secondRow.appendChild(sixButton);
+        thirdRow.appendChild(sevenButton);
+        thirdRow.appendChild(eightButton);
+        thirdRow.appendChild(nineButton);
+        fourthRow.appendChild(zeroButton);
+        section.appendChild(equationExpression);
